839cf1b70c64c44ec8141f0ab1fb805ed07dd020
Kristopher Chun <kristopher.chun@dataq.ai>
2019-05-06 15:27:53 -0700

    .

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
-  name = 'laravelValidation',
-  packages = ['laravelValidation'], 
+  name = 'validation',
+  packages = ['validation'], 
   version = '0.1',
   description = 'Laravel like data validation library for python language',
   author = 'Walid Mashal',
   author_email = 'walidmashal4@gmail.com',
-  url = 'https://github.com/kristopherchun/laravel-validation.git', 
+  url = 'https://github.com/kristopherchun/validation.git', 
   download_url = '',
   keywords = ['validation', 'python', 'laravel'],
   classifiers = [],
